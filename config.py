d1e6b558a054aa980e4d7faeed2fe134bb119ee1
gusbarron <67945700+gusbarron@users.noreply.github.com>
2022-06-21 13:14:23 -0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+import pymongo
+import certifi
+
+con_str = "mongodb+srv://gusbarron:<LACIEgus11!!>@cluster0.lbhklne.mongodb.net/?retryWrites=true&w=majority"
+
+client = pymongo.MongoClient(con_str, tlsCAFile=certifi.where())
+
+db = client.get_database("Candystore")
+
