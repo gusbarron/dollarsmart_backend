9a5fcf627d627e0cdab2603541dbebff60cc0f6a
gusbarron <67945700+gusbarron@users.noreply.github.com>
2022-06-21 20:08:15 -0700

    Add files via upload

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import pymongo
 import certifi
 
-con_str = "mongodb+srv://gusbarron:<LACIEgus11!!>@cluster0.lbhklne.mongodb.net/?retryWrites=true&w=majority"
+con_str = "mongodb+srv://gusbarron:LACIEgus11!!@cluster0.lbhklne.mongodb.net/?retryWrites=true&w=majority"
 
 client = pymongo.MongoClient(con_str, tlsCAFile=certifi.where())
 
